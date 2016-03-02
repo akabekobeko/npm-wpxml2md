c27912325a190ff070eaa4a20178c0f27f84b45f
akabeko <akabekobeko@gmail.com>
2016-03-02 19:10:22 +0900

    #18 Implement a parse of the WordPress Shorcode

---
+++ test/lib/shortcode.test.js
@@ -0,0 +1,66 @@
+import assert from 'power-assert';
+import Shortcode from '../../src/lib/shortcode.js';
+import { SHCodes } from '../../src/lib/shortcode.js';
+
+/** @test {Shortcode} */
+describe( 'Shortcode', () => {
+  /** @test {Shortcode#convert} */
+  describe( 'convert', () => {
+    it( '[code]', () => {
+      const text     = '[code]\nCode\n[/code]';
+      const actual   = Shortcode.convert( text );
+      const expected = '\n\n```\nCode\n```\n\n';
+      assert( actual === expected );
+    } );
+
+    it( '[code lang="lang"]', () => {
+      const text     = '[code lang="lang"]\nCode\n[/code]';
+      const actual   = Shortcode.convert( text );
+      const expected = '\n\n```lang\nCode\n```\n\n';
+      assert( actual === expected );
+    } );
+
+    it( '[lang]', () => {
+      let text     = '[plain]\nCode\n[/plain]';
+      let actual   = Shortcode.convert( text );
+      let expected = '\n\n```\nCode\n```\n\n';
+      assert( actual === expected );
+
+      text     = '[text]\nCode\n[/text]';
+      actual   = Shortcode.convert( text );
+      assert( actual === expected );
+
+      for( let i = 3, max = SHCodes.length; i < max; ++i ) {
+        const code = SHCodes[ i ];
+        text = '[' + code + ']Code[/' + code + ']';
+        actual   = Shortcode.convert( text );
+        expected = '\n\n```' + code + '\nCode\n```\n\n';
+        assert( actual === expected );
+      }
+    } );
+  } );
+
+  /** @test {Shortcode#parse} */
+  describe( 'parse', () => {
+    it( 'parse', () => {
+      const text   = 'caption id="ID" align="alignright" width="300" caption="Caption"';
+      const actual = Shortcode.parse( text );
+      assert( actual.code           === 'caption' );
+      assert( actual.params.id      === 'ID' );
+      assert( actual.params.align   === 'alignright' );
+      assert( actual.params.width   === '300' );
+      assert( actual.params.caption === 'Caption' );
+    } );
+  } );
+
+  /** @test {Shortcode#trimLineBreak} */
+  describe( 'trimLineBreak', () => {
+    it( 'trimLineBreak', () => {
+      let actual = Shortcode.trimLineBreak( '\nText\n' );
+      assert( actual === 'Text' );
+
+      actual = Shortcode.trimLineBreak( '\n\nText\n' );
+      assert( actual === '\nText' );
+    } );
+  } );
+} );
