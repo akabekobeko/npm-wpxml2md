acd6877689734a17f1f4076a2ef8701be59d05af
akabeko <akabekobeko@gmail.com>
2016-03-04 20:48:16 +0900

    #3 Implement a command line interface

--- src/lib/typedef.js
+++ src/lib/typedef.js
@@ -6,14 +6,26 @@
  * convert the HTML element to Markdown.
  *
  * @typedef {Object} MdConverter
- * @property {String|Array|Function} filter Filter to check the HTML tags.
- * @property {Function} replacement Function to replace the DOM Node.
+ * @property {String|Array|Function} filter      Filter to check the HTML tags.
+ * @property {Function}              replacement Function to replace the DOM Node.
  */
 
  /**
   * Optional settings of this tool.
   *
   * @typedef {Object} Option
-  * @property {Boolean} gmf Enable a GitHub Flavored Markdown. Default is disable.
+  * @property {Boolean}   gmf        Enable a GitHub Flavored Markdown. Default is disable.
   * @property {Converter} converters Extended converter collection.
   */
+
+/**
+ * Commad line options.
+ *
+ * @typedef {Object} CLIOptions
+ * @property {Boolean} help    Mode to display the help text.
+ * @property {Boolean} version Mode to display the version number.
+ * @property {String}  input   Path of the SVG file or PNG file directory.
+ * @property {String}  output  Path of the output directory.
+ * @property {Boolean} gfm     Enable a GitHub Flavored Markdown. Default is disable.
+ * @property {Boolean} report  Display the process reports. Default is disable.
+ */
