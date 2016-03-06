85b2d1668bad864298731a726c5aba69be828d96
akabeko <akabekobeko@gmail.com>
2016-03-06 14:47:27 +0900

    #29 Add an examples

---
+++ examples/no-gfm.js
@@ -0,0 +1,13 @@
+const wpxml2md = require( 'wpxml2md' );
+const util = require( './util.js' );
+
+const dest = './dest';
+util.mkdirSync( dest );
+
+wpxml2md( 'wp.xml', dest, { report: true, noGFM: true } )
+.then( () => {
+  console.log( 'Completed!!!' );
+} )
+.catch( ( err ) => {
+  console.error( err );
+} );
