40df7645884db8384c08763e5118430fa7a044b8
akabeko <akabekobeko@gmail.com>
2018-10-03 20:54:12 +0900

    #66 Use Babel 7

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = (api) => {
+  const presetEnv = [
+    '@babel/preset-env',
+    {
+      targets: {
+        node: '8'
+      }
+    }
+  ]
+
+  return {
+    presets: api.env('development') ? [
+      presetEnv,
+      'power-assert'
+    ] : [
+      presetEnv
+    ]
+  }
+}
